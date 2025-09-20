060f844f73f547ba47f768ffc05cf111cad837a9
Massimo De Lorenzo <massimo.delorenzo@proton.me>
2025-09-20 18:07:00 +0200

    two sum problem resolution

--- src/two-sum/two-sum.spec.ts
+++ src/two-sum/two-sum.spec.ts
@@ -4,6 +4,10 @@ import {twoSum} from "./two-sum";
 describe('TwoSum', () => {
 
     it('Input: nums = [2,7,11,15], target = 9, Output: [0,1]', () => {
-        expect(twoSum([2,7,11,15], 9)).toBe([0,1]);
+        expect(twoSum([2,7,11,15], 9)).toStrictEqual([0,1]);
     });
+
+    it('Input: nums = [1,2,3], target = 100, Output: [-1,-1]', () => {
+        expect(twoSum([1,2,3], 100)).toStrictEqual([-1,-1]);
+    })
 });
--- src/two-sum/two-sum.ts
+++ src/two-sum/two-sum.ts
@@ -1,3 +1,18 @@
 export function twoSum(nums: number[], target: number): number[] {
-    return []
-};
+
+    const seen = new Map<number,number>();
+
+    for (let i = 0; i < nums.length; i++) {
+
+        const keys = [...seen.keys()];
+        const current = nums[i];
+        const need = target - current;
+
+        if (keys.includes(need)) {
+            return [seen.get(need) as number, i]
+        }
+        seen.set(current, i);
+    }
+
+    return [-1, -1];
+}
